4a7d8541096b4536bb4a8d40667885a5a54fb849
Jason Hine <jhine@esri.com>
2017-08-03 09:07:54 -0600

    Initial commit

---
+++ widgets/FractalDimension/nls/strings.js
@@ -0,0 +1,20 @@
+﻿define({
+    root:({
+        "intro": "To measure the fractal dimension of a feature,<ol><li>Set Target Layer to the feature's layer.</li><li>Click Select Feature.</li><li>Click a feature.</li></ol>",
+        "labelInputs": "Parameters",
+        "labelLayer": "Target Layer",
+        "labelSelectLayer": "-- select --",
+        "labelLayerNotFound": "-- no feature layers found --",
+        "labelPasses": "Passes to make",
+        "btnLabelSelect": "Select Feature",
+        "btnLabelCancelSel": "Cancel Selection",
+        "btnLabelCancelAna": "Cancel Analysis",
+        "btnLabelClear": "Clear Results",
+        "labelWorking": "Working...",
+        "labelCancelled": "Analysis cancelled",
+        "labelResults": "Results",
+        "labelDimension": "Dimension",
+        "labelRSquared": "R-squared",
+        "chartNotes": "Dimension is calculated as the slope of the regression line for green points. (Gray points are ignored to reduce bias.)"
+    }) 
+});
